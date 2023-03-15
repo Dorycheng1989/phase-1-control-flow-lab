6f41eb0a7a773ad06ba4495c94988c9eacf36388
Doreen Odhiambo <dorychieng@gmail.com>
2023-03-15 23:57:04 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet()
+  if (ride <= 400) {
+
+    return ("This one is on me!");
+
+  } else if (ride > 400 && ride <= 2000) {
+
+    return ("That will be 20 bucks.");
+
+  } else if (ride > 2000 && ride <2500) {
+
+    return ("I will gladly take your thirty bucks.");
+
+  } else 
+
+    return ("No can do.");(){
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity()
+  if (city == "NYC") {
+
+    return ("Ok, sounds good.");
+
+  } else {
+  
+    return ('No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip()
+  if (tip == "generous") {
+
+    return ("Thank you so much.");
+
+  } else if (tip == "not as generous") {
+
+    return ('Thank you.');
+
+  } else {
+
+    return ("Bye.");
 }
